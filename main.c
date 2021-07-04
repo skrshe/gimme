02a501b9391c0e91d9077383ac7562a626080b41
skrshe <sanshonekrashe@gmail.com>
2021-07-04 17:14:14 +1000

    app user prompt

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
 #define C_MAGENTA "\x1b[35m"
@@ -9,12 +10,23 @@
 
 #include "help.h"
 
+char *prompt() {
+    char * input = NULL;
+    size_t len;
+
+    printf("> ");
+
+    getline(&input, &len, stdin);
+    input[strcspn(input, "\n")] = 0;
+
+    return input;
+}
 
 int main(int argc, char **argv)
 {
     for (int i = 1; i < argc; ++i) {
         if (strcmp(argv[i], "spawn") == 0) {
-            printf("create a repo at <domain/user/repo>\n");
+            printf("create or clone a repo to <domain/user/repo>\n");
         } else if (strcmp(argv[i], "modify") == 0) {
             printf("set <option> to <value> on <repo>\n");
         } else if (strcmp(argv[i], "config") == 0) {
@@ -27,6 +39,11 @@ int main(int argc, char **argv)
             printf("open a <git porcelain ui> on the current git directory\n");
         } else if (strcmp(argv[i], "--help") == 0) { usage(stdout);
         } else if (strcmp(argv[i], "-h") == 0) { usage(stdout);
+        } else if (strcmp(argv[i], "prompt") == 0) {
+            printf("enter something:\n");
+            printf("you entered %s\n", prompt());
+
+
         } else if (argc > 1){
             const char *other_arg = argv[1];
 
